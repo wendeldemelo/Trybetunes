eaad19702e1347f27ff2cccd863f8d2fac417a3d
wendeldemelo <wendeldemelo@outlook.com>
2022-06-23 17:07:10 -0400

    feat: login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Navigate } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
+
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      loggedIn: false,
+      formFulfilled: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
+  async handleSubmit() {
+    const { name } = this.state;
+    this.setState({ loggedIn: true });
+    await createUser({ name });
+    this.setState({
+      loggedIn: false,
+      formFulfilled: true,
+    });
+  }
+
+  render() {
+    const { name, loggedIn, formFulfilled } = this.state;
+    const MIN_CARACTHERS = 3;
+    if (loggedIn) return <Loading />;
+    if (formFulfilled) return { component: () => <Navigate to="/search" /> };
+    return (
+      <section data-testid="page-login">
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="input-name">
+            <input
+              data-testid="login-name-input"
+              type="text"
+              onChange={ this.handleChange }
+              value={ name }
+              name="name"
+              placeholder="Nome"
+            />
+          </label>
+          <label htmlFor="input-button">
+            <input
+              data-testid="login-submit-button"
+              type="submit"
+              value="Entrar"
+              disabled={ name.length < MIN_CARACTHERS }
+            />
+          </label>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default Login;
