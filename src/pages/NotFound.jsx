1bcfedf47fb6f1ec8667051f8040e9db8fc3e3b3
wendeldemelo <wendeldemelo@outlook.com>
2022-07-02 23:40:21 -0400

    style: not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
 
 class NotFound extends Component {
   render() {
     return (
-      <main data-testid="page-not-found">
-        <h1>Page Not Found</h1>
-      </main>
+      <Alert variant="danger" dismissible>
+        <Alert.Heading>Oops! Erro!</Alert.Heading>
+        <p>
+          Página não encontrada. Mude a rota e tente novamente.
+        </p>
+      </Alert>
     );
   }
 }
